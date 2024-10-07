772f196cb0110c7b9057de771a8289c012875c17
songshenglin <13639120050@163.com>
2024-10-07 20:12:10 +0800

    邮件未申请地址-删除-2

--- 02-crane/9月/03-二期申请/01-技术方案/设计接口/非流程/1.java
+++
@@ -1,5 +0,0 @@
-List<PermissionApplyResult> fetchByCondition(@Param("condition") ApplyResultCondition condition);
-
-  List<PermissionApplyResult> fetchByExpireTimeAndStatus(@Param("expireTimeEnd") LocalDateTime expireTimeEnd, @Param("status") List<PermissionApplyResultStatus> status);
-
-  List<PermissionApplyResult> fetchByExpireTimeAndStatusAndBusinessType(@Param("expireTimeEnd") LocalDateTime expireTimeEnd, @Param("status") List<PermissionApplyResultStatus> status, @Param("permissionApplyType") Integer permissionApplyType);
